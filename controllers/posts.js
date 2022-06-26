6f47ee95011bef0f1d1cacec899f4048328c339b
Aniket Chaudhri <anichaudhri@gmail.com>
2022-06-26 16:55:52 +0530

    Add postsByUser

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,6 +7,7 @@ let uuidv1 = require('uuidv1')
 
 exports.getPosts = (req, res) => {
     const posts = Post.find()
+    .populate("postedBy", "_id name")
     .select("_id title body")
     .then(posts => {
         res.status(200).json({posts: posts})
@@ -28,7 +29,7 @@ exports.createPost = (req, res) => {
         // req.profile.posts.push(post);
         req.profile.hash_password = undefined;
         req.profile.salt = undefined;
-        
+
         post.postedBy = req.profile;
         if (files.photo) {
             post.photo.data = fs.readFileSync(files.photo.path);
@@ -43,4 +44,19 @@ exports.createPost = (req, res) => {
             res.json(result);
         })
     })
+}
+
+exports.postsByUser = (req, res) => {
+    Post.find({postedBy: req.profile._id})
+    .populate("postedBy", "_id name")
+    .select("_id title body")
+    .exec((err, posts) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            })
+        }
+        res.json(posts);
+    }
+    )
 }
