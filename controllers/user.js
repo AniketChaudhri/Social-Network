9c24b4ed175e7040f0604fd61489acc14bbdf2ab
Aniket Chaudhri <anichaudhri@gmail.com>
2022-06-26 15:40:50 +0530

    Show single user

--- controllers/user.js
+++ controllers/user.js
@@ -32,3 +32,9 @@ exports.allUsers = (req, res) => {
         res.json(users);
     }).select("name email created updated");
 }
+
+exports.getUser = (req, res) => {
+    req.profile.hash_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+}
